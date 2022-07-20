2f1145083c9c60fc82ce34b9d45af86fe0102415
fahmir2 <ezequieldiglesias@gmail.com>
2022-07-19 23:05:15 -0300

    html agregado como res

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express(); 
+const path = require('path');
+
 app.listen(3000, ()=>{
     console.log("Servidor corriendo")
 })
 app.get('/', function(req, res){
-    res.send("Bienvenidos al sitio")
+    res.sendFile(path.join(__dirname, '/views/index.html'));
 })
-app.get("/home", function(req, res){
-    res.send("Estamos en el home")
-})
+
